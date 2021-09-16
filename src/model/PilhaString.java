d6d2d323482911c62ef0f9d0d9cee9b8dade32cf
Renan Parabre <renan.parabre@gmail.com>
2021-09-16 17:09:15 -0300

    Commit

---
+++ src/model/PilhaString.java
@@ -0,0 +1,60 @@
+package model;
+
+public class PilhaString {
+	
+	NoString topo;
+
+	public PilhaString() {
+		topo = null;
+	}
+	
+	public boolean isEmpty() {
+		if (topo == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void push(String e) {
+		NoString elemento = new NoString();
+		elemento.dado = e;
+		if (isEmpty()) {
+			topo = elemento;
+		} else {
+			elemento.proximo = topo;
+			topo = elemento;
+		}
+	}
+	
+	public String pop() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Não há elementos para desempilhar");
+		}
+		String valor = topo.dado;
+		topo = topo.proximo;
+		return valor;
+	}
+	
+	public String top() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Não há elementos na pilha");
+		}
+		String valor = topo.dado;
+		return valor;
+	}
+	
+	public int size() {
+		int cont = 0;
+		if (!isEmpty()) {
+			NoString auxiliar = topo;
+			cont = 1;
+			while (auxiliar.proximo != null) {
+				auxiliar = auxiliar.proximo;
+				cont++;
+			}
+		}
+		return cont;
+	}
+
+}
